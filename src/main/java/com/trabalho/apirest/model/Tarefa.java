e00d263cb30e9fbd6f79d24acd1f634b0ebefcf3
Rudy Gomes <rudygomes19@gmail.com>
2025-08-26 01:41:12 -0300

    Tarefa, TarefaController, TaraefaReposiory e application, properties finalizados

---
+++ src/main/java/com/trabalho/apirest/model/Tarefa.java
@@ -0,0 +1,73 @@
+package com.trabalho.apirest.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@DateTimeFormat(iso= DateTimeFormat.ISO.DATE)
+	private LocalDate dataEntrega;
+	private String responsavel;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		dataEntrega = dataEntrega;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	@Override
+	public String toString() {
+		return "Tarefa [id=" + id + ", nome=" + nome + ", DataEntrega=" + dataEntrega + ", responsavel=" + responsavel
+				+ "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(dataEntrega, id, nome, responsavel);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tarefa other = (Tarefa) obj;
+		return Objects.equals(dataEntrega, other.dataEntrega) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome) && Objects.equals(responsavel, other.responsavel);
+	}
+	
+	
+	
+}
